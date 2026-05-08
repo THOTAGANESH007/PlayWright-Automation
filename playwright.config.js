a747a5a8a35754a7372e84cbb12b439c879547c0
THOTAGANESH007 <ganeshthota656@gmail.com>
2026-05-08 18:20:18 +0530

    config file

---
+++ playwright.config.js
@@ -0,0 +1,21 @@
+import { defineConfig} from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests', // Where the actual tests are located
+
+  timeout: 77 * 1000, // explicit timeout globally
+
+  expect :{
+    timeout : 5000, // timeout only for the expect (Assertions)
+  },
+  
+  reporter : 'html',
+  use: {
+    browserName:'chromium', // Default Browser Configuration to run the project 
+    // browserName:'firefox',
+    // browserName:'webkit',
+    headless:false, // runs in headed mode
+  }
+
+});
+
